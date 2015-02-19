e9662a51be74ec61c009999d6b65afdea7bbf06c
davis dulin <davisdulin@gmail.com>
2015-02-18 16:50:51 -0800

    Initial Commit

---
+++ Team Project 22C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Team Project 22C
+//
+//  Created by Davis Dulin on 2/18/15.
+//  Copyright (c) 2015 Liam Purvis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
